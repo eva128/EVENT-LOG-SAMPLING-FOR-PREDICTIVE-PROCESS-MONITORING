470ef16d094fc936bb516657e982b8397dfb033a
Eva <44433196+eva128@users.noreply.github.com>
2024-07-16 19:05:57 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+# EVENT LOG SAMPLING FOR PREDICTIVE PROCESS MONITORING
+## HOW DOES SAMPLING AFFECT THE RESULTS OF NEXT ACTIVITY PREDICTION IN PREDICTIVE PROCESS MONITORING?
+
+This repositry contains the implementation and data used for my master thesis. 
+
+The goal was improve the generalization of prediction models in the context of predictive process monitoring. Therefore, three different sampling techniques were tested on five event logs, aiming to decrase the example leakage between train and test log. 
+
+The code contains implementations from different sources. The implementations are closely following the original code with small adaptions, so that the different parts work together.
+
+Detailed results can be found in the folder "results", including a csv-file summarizing all executions.
+
+
+### How to run the code
+
+1. Clone repository
+   - ```git clone ```
+
+2. Install dependencies (Python 3.8.8)
+   - ```pip install -r requirements.txt```
+
+3. How to:
+    1. Run the script lstm.py with the following flags
+        - *--task*: "next_activity", 
+        - *--contextual_info*: "True" if you want to exploit contextual features, "False" otherwise (default: False),
+        - *--transition*: "True" if the dataset contains transition information, "False" otherwise (default: False),
+        - *--learning_rate*: learning rate of optimization algorithm (default: 0.002),
+        - *--num_folds*: number of folds in validation (default: 10),
+        - *--batch_size*: training batch size of models (default: 256),
+        - *--data_dir*: directory where the dataset is located,
+        - *--data_set*: name of the dataset,
+        - *--checkpoint*: directory where the models are saved (default: "./checkpoints/"),
+        - *--control_flow_p*: "True" if you want to use control-flows as features, "False" otherwise (default: True),
+        - *--time_p*: "True" if you want to use time as features, "False" otherwise (default: True),
+        - *--resource_p*: "True" if you want to use resource as features, "False" otherwise (default: False),
+        - *--data_p*: "True" if you want to use data as features, "False" otherwise (default: False),
+        - *--result_dir*: directory where the experimental results are saved
+        - *--sampling_technique*: the desired sampling technique ("random", "relevance", "variant", "none").
+                                  - If you want to use variant-based sampling please provide the sampled event log with the following name: *dataset*_variant, e.g., "bpi2012_variant"
+                                  - If you want to use relevance-based sampling please provide the evetn log as a xes-file and a petri net of your event log, 
+                                  e.g., bpi2012.xes and bpi2012.pnml
+      - e.g., 
+        - ```python lstm.py --task "next_activity" --contextual_info False --learning_rate 0.002 --num_epochs 15 --batch_size 128 --data_dir "./samples/" --data_set "helpdesk" --checkpoint_dir "./checkpoints/" --control_flow_p True --time_p False --resource_p False --data_p False --transition False --result_dir "./result/" --sampling_technique "relevance"``
+
+
+### Citations:
+Implementation of the LSTM neural networks:
+The fundamental implementation and structure is from Fani Sani et al. This includes all folder unless dated otherwise.
+The file lstm.py is based on next_activity_prediction_with_neural_networks.py and code was incorporated to include the chosen sampling methods and the calculation of the example leakage.
+Fani Sani, M., Vazifehdoostirani, M., Park, G., Pegoraro, M., Van Zelst, S. J., & Van Der Aalst, W. M. P. (2023). Performance-preserving event log sampling for predictive monitoring. J Intell Inf Syst, 61(1), 53–82. https://doi.org/10.1007/s10844-022-00775-9
+
+Relevance-based Sampling:
+The folder "Relevance_based_sampling" contains the necessary code for this sampling technique. 
+All classes were implemented by:
+Kabierski, M., Nguyen, H. L., Grunske, L., & Weidlich, M. (2021). Sampling What Matters: Relevance-guided Sampling of Event Logs. 2021 3rd International Conference on Process Mining (ICPM), 64–71. https://doi.org/10.1109/ICPM53251.
+2021.9576875
+
+Variant-based Sampling:
+Plug-In in ProM - Package LogFiltering
+Fani Sani, M., Vazifehdoostirani, M., Park, G., Pegoraro, M., van Zelst, S. J., & van der Aalst, W. M. (2021). Event log sampling for predictive monitoring. International Conference on Process Mining, 154–166. Retrieved December 18, 2023, from https://library.oapen.org/bitstream/handle/20.500.12657/54026/978- 3- 030-98581-3.pdf?sequence=1#page=167
+
+Example leakage calculations:
+Abb, L., Pfeiffer, P., Fettke, P., & Rehse, J.-R. (2023). A Discussion on Generalization in Next-Activity Prediction [arXiv:2309.09618 [cs]]. Retrieved December 11, 2023, from http://arxiv.org/abs/2309.09618
+
+
+Event logs:
+- bpi2012: van Dongen, B.: BPI Challenge 2012. https://data.4tu.nl/articles/_/12689204/1
+- bpi2013: Steeman, W.: BPI Challenge 2013, incidents. https://data.4tu.nl/articles/_/12693914/1
+- bpi2017: van Dongen, B.: BPI Challenge 2017. https://data.4tu.nl/articles/_/12696884/1
+  bpi2017 contains the half of the original event log, because too much memory space is needed otherwise
+- helpdesk: Verenich, I.: Helpdesk event log. https://doi.org/10.17632/39bp3vv62t.1
+- mobis2019: Scheid, M., Rehse, J.R., Houy, C., Fettke, P.: Data set for mobis challenge 2019 (2018). https://doi.org/10.13140/RG.2.2.11870.28487
+
+Note: Sampling and model training may take several hours for the event logs bpi2012 and bpi2017.
+
+
+### Requirements
+
+- Datetime format: "%Y.%m.%d %H:%M".
+- The input csv file contains the columns named after the xes elements, e.g., concept:name
+- Csv format dataset where 'case:concept:name' denotes the caseid, 'concept:name' the activity,'org:resource' the resource, 'time:timestamp' the complete timestamp, 'event_@' the event attribute, and 'case_@' the case attribute.
---
+++ lstm.py
@@ -0,0 +1,290 @@
+# added comments to what is performed
+import xml.etree.ElementTree as ET
+import pm4py
+import traceback
+import pickle
+import os.path
+from Relevance_based_Sampling.SamplingAlgorithms import FeatureGuidedLogSampler, SequenceGuidedLogSampler
+import pred_config
+from core.feature_encoder import FeatureEncoder
+from core.feature_generator import FeatureGenerator
+from core.model import net
+import time
+import numpy as np
+import csv
+import random
+from array import array
+import pandas as pd
+from sklearn.model_selection import GroupShuffleSplit, RandomizedSearchCV, train_test_split
+
+if __name__ == '__main__':
+    args = pred_config.load()
+
+    contextual_info = args.contextual_info
+    status = args.status
+    task = args.task
+    control_flow_p = args.control_flow_p
+    time_p = args.time_p
+    resource_p = args.resource_p
+    data_p = args.data_p
+    transition = args.transition
+
+    data_dir = args.data_dir
+    data_set = args.data_set
+    
+    num_epochs = args.num_epochs
+    batch_size = args.batch_size
+
+    checkpoint_dir = args.checkpoint_dir
+    
+    cross_number = args.cross_number
+
+    exp_name = '%s_%s_%s_%s_%s_%s_%s_%s_LSTM' % (data_set, task, control_flow_p, time_p, resource_p, data_p, num_epochs, batch_size)
+
+    load_saved_data = args.load_saved_data
+    load_saved_test_data = args.load_saved_test_data
+    save_data = args.save_data
+
+    total_results_file = args.result_dir + exp_name + '.csv'
+    file_exists = os.path.isfile(total_results_file)
+
+    sampling_technique = args.sampling_technique
+
+    # create or open file to store the results
+    with open (total_results_file, 'a') as csvfile:
+        headers = ['Dataset', 'Sampling Method', 'Accuracy', 'Example Leakage', 'Running_Time', 'F1_Score', 'Precisions', 'Recalls', 'FeatureEncoder_Time',
+              'TruePositive', 'FalsePositive', 'FalseNegative', 'TrueNegative']
+        writer = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+        print("File created")  
+        if not file_exists:
+            writer.writeheader()  # file doesn't exist yet, write a header
+
+    # get data set
+    filename = data_dir + data_set + ".csv" 
+    model_name = '%s_%s_%s_%s_%s_%s_%s_%s' % (data_set, task, control_flow_p, time_p, resource_p, data_p, num_epochs,
+                batch_size)
+
+    feature_name = '%s_%s_%s_%s_%s_%s' % (
+                    data_set, task, control_flow_p, time_p, resource_p, data_p)
+
+    log_config = {"control_flow_p": control_flow_p, "time_p": time_p, "resource_p": resource_p, "data_p": data_p, 'transition': transition}
+
+    # feature generation and split data set
+    print("flag: loading training data")
+    Start_FeatureGenerator = time.time()
+    fg = FeatureGenerator()
+    df = fg.create_initial_log(filename, log_config)
+
+    #SAMPLING + splitting
+    print("Sampling and splitting data in train/test set")
+    print("Sampling: ", sampling_technique)
+    
+    #Random Sampling (own implementation) size:
+    if(sampling_technique=="random"):
+        list_ids = list(df['id'].unique())
+        list_ids = [int(id) for id in list_ids]
+        list_ids = array("i", list_ids)
+        sample_size = int(len(list_ids)*0.1)
+        sampled_ids = random.sample(list_ids, sample_size)
+
+        # train/test split
+        train_ids_size = int(len(sampled_ids)*0.8)
+        train_ids = random.sample(sampled_ids, train_ids_size)
+        test_ids= []
+        for id in sampled_ids:
+            if (id not in train_ids):
+                test_ids.append(id)
+        train_df =  df[df['id'].isin(train_ids)]
+        test_df = df[df['id'].isin(test_ids)]
+    
+
+    # Relevance-based sampling (based on the implemention of Kabierski et al., 2020)
+    elif(sampling_technique=="relevance"):
+        xes_file_path = data_dir + data_set + ".xes"
+        xes = pm4py.read_xes(xes_file_path) 
+
+        xes = pm4py.convert_to_event_log(xes, case_id_key="case:concept:name")
+        sample_size = int(len(xes) * 0.5)
+
+        petrinet, initial_marking, final_marking = pm4py.read_pnml(data_dir + data_set + ".pnml")
+        sampler = SequenceGuidedLogSampler(xes, batch_size=1)
+        sample = sampler.construct_sample(xes, petrinet, initial_marking, final_marking, sample_size)
+        ids = [trace.attributes['concept:name'] for trace in sample]
+        ids = [int(id) for id in ids]
+        relevance_df = df[df['id'].isin(ids)]
+        
+        # train/test split
+        sampled_ids = list(relevance_df['id'].unique())
+        sampled_ids = [int(id) for id in sampled_ids]
+        sampled_ids = array("i", sampled_ids)
+        train_ids_size = int(len(sampled_ids)*0.8)
+        train_ids = random.sample(sampled_ids, train_ids_size)
+        test_ids= []
+        for id in sampled_ids:
+            if (id not in train_ids):
+                test_ids.append(id)
+        train_df =  df[df['id'].isin(train_ids)]
+        test_df = df[df['id'].isin(test_ids)]
+
+
+    # Variant-based sampling:
+    # The variant-based sampling was done using a plug-in in ProM, 
+    # this code simply performs a train/test split of the sampled event log 
+    elif(sampling_technique=="variant"):
+        xes_file_path = data_dir + data_set + "_variant.xes"
+        xes = pm4py.read_xes(xes_file_path) 
+        
+        variant_df = pm4py.convert_to_dataframe(xes)
+        
+        sampled_ids = list(variant_df['case:concept:name'].unique())
+        sampled_ids = [int(id) for id in sampled_ids]
+        sampled_ids = array("i", sampled_ids)
+        train_ids_size = int(len(sampled_ids)*0.8)
+        train_ids = random.sample(sampled_ids, train_ids_size)
+        test_ids= []
+        for id in sampled_ids:
+            if (id not in train_ids):
+                test_ids.append(id)
+        train_df =  df[df['id'].isin(train_ids)]
+        test_df = df[df['id'].isin(test_ids)]
+
+    # No samping, just train-test split (80/20)
+    else: 
+        #split
+        list_ids = list(df['id'].unique())
+        train_ids_size = int(len(list_ids)*0.8)
+        train_ids = random.sample(list_ids, train_ids_size)
+        train_df = df[df['id'].isin(train_ids)]
+        test_df = df[~df['id'].isin(train_ids)]
+   
+
+    columns = ['concept:name', 'concept:name']
+    if(args.time_p):
+        columns = ['concept:name', 'concept:name', 'time:timestamp']
+    header_row = pd.DataFrame([columns], columns=test_df.columns)
+    train_df = train_df.reset_index(drop=True)
+    test_df = test_df.reset_index(drop=True)    
+
+    print("Train + test set: ")
+    print(train_df.shape)
+    print(test_df.shape)
+
+    FeatureGenerator_Time = time.time() - Start_FeatureGenerator
+    print("FeatureGenerator done")
+
+    num_events = len(df)
+    num_cases = len(set(df["id"]))
+
+    print("flag: generating features")
+    if task == 'next_activity':
+        loss = 'categorical_crossentropy'
+        regression = False
+        feature_type_list = ["activity_history"]
+        train_df = fg.add_activity_history(train_df)
+        train_df = fg.add_next_activity(train_df)
+
+    # training set generation
+    print("flag: encoding features")
+    Start_FeatureEncoder = time.time()
+    fe = FeatureEncoder()
+    train_X, train_y = fe.original_one_hot_encode(train_df, feature_type_list, task, feature_name)
+    FeatureEncoder_Time = time.time() - Start_FeatureEncoder
+    print("done")
+
+    #if contextual_info:
+        # train_df = fg.queue_level(train_df)
+        # activity_list = fg.get_activities(train_df)
+        # train_context_X = fg.generate_context_feature(train_df, activity_list)
+        # model = net()
+        # if task == 'next_timestamp':
+        #     model.train(train_X, train_y, regression, loss, n_epochs=num_epochs, batch_size=batch_size,
+        #                 model_name=model_name, checkpoint_dir=checkpoint_dir,
+        #                 X_train_ctx=train_context_X)
+        # elif task == 'next_activity':
+        #     model.train(train_X, train_y, regression, loss, n_epochs=num_epochs, batch_size=batch_size,
+        #                 model_name=model_name, checkpoint_dir=checkpoint_dir,
+        #                 X_train_ctx=train_context_X)
+    #else:
+
+    train_context_X = None
+    model = net()
+
+    if task == 'next_activity':
+        model.train(train_X, train_y, regression, loss, n_epochs=num_epochs, batch_size=batch_size,
+                    model_name=model_name, checkpoint_dir=checkpoint_dir,
+                    context=contextual_info)
+
+    Running_Time = model.running_time
+    print("training is done")
+    model.load(checkpoint_dir, model_name=model_name)
+
+    ## loading test data
+    print("testing")
+    fg = FeatureGenerator()
+    num_events = len(df)
+    num_cases = len(set(df["id"]))
+
+    # feature generation
+    print("flag: generating features")
+    if task == 'next_activity':
+        test_df = fg.add_activity_history(test_df)
+        test_df = fg.add_next_activity(test_df)
+    print("done")
+
+    # test set generation
+    print("flag: encoding features")
+    fe = FeatureEncoder()
+    test_X, test_y = fe.preprocessed_one_hot_encode(test_df, feature_type_list, task, feature_name)
+    print("done")
+    
+    exp_info = {"task": task, "filename": filename, "control_flow_p": control_flow_p, "time_p": time_p,
+                        "resource_p": resource_p, "data_p": data_p, "num_epochs": num_epochs,
+                        "batch_size": batch_size}
+
+
+    # Evaluate the model on the test data using `evaluate`
+    CF_matrix, report, Accuracy, F1_Score, Precision, Recall = model.evaluate(test_X, test_y, exp_info)
+
+    TruePositive = sum(np.diag(CF_matrix))
+    FalsePositive = 0
+    for jj in range(CF_matrix.shape[0]):
+        FalsePositive += sum(CF_matrix[:, jj]) - CF_matrix[jj, jj]
+    FalseNegative = 0
+    for ii in range(CF_matrix.shape[0]):
+        FalseNegative += sum(CF_matrix[ii, :]) - CF_matrix[ii, ii]
+    TrueNegative = 0
+    for kk in range(CF_matrix.shape[0]):
+        temp = np.delete(CF_matrix, kk, 0)
+        temp = np.delete(temp, kk, 1)
+        TrueNegative += sum(sum(temp))
+
+    # Calculate example leakage (citation abb ...)
+    test_prefix_list = [row['activity_history'] for _, row in test_df.iterrows()]
+    train_prefix_list = [row['activity_history'] for _, row in train_df.iterrows()]
+    leaked = 0
+    for activity_history in test_prefix_list:
+        if (activity_history in train_prefix_list):
+            leaked += 1
+    leakage_percent = leaked / len(test_prefix_list)
+    
+    print("Leaked: ",leaked)
+    print("Out of ", len(test_prefix_list))
+    print(leakage_percent)
+
+
+    Results_data = [data_set, sampling_technique, Accuracy, leakage_percent, Running_Time, F1_Score, Precision, Recall, FeatureGenerator_Time,
+                    TruePositive, FalsePositive, FalseNegative, TrueNegative]
+
+    print("flag: saving output")
+    with open(total_results_file, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(Results_data)
+    print("done")
+
+    
+
+
+ 
+          
+
+
